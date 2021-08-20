7d1145c4d4e547f402fed62c644d52730625ffc9
Harshvardhan <67060748+HarshVardhan22@users.noreply.github.com>
2021-08-21 01:23:52 +0530

    +implemented logic

---
+++ index.js
@@ -0,0 +1,25 @@
+const translateBtn = document.getElementById("translateBtn");
+
+translateBtn.addEventListener("click", handleBtnClick);
+
+const inputContainer = document.getElementById("inputTextArea");
+const outputContainer = document.querySelector("#result");
+
+const url = "https://api.funtranslations.com/translate/british.json";
+
+function handleBtnClick(event) {
+  const inputText = inputContainer.value;
+  const urlGenerated = urlGenerator(inputText);
+  console.log(urlGenerated);
+  fetch(urlGenerated)
+    .then((response) => response.json())
+    .then((json) => {
+      outputContainer.innerText = json.contents.translated;
+    })
+    .catch((err) => alert(err.message));
+}
+
+function urlGenerator(inputText) {
+  const uri = encodeURI(inputText);
+  return `${url}?text=${uri}`;
+}
